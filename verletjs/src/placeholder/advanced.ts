d3940e81f5b2efa35fc03ebc1b6a506c86881e3b
SriGanesh01 <psriganesh2005@gmail.com>
2025-07-10 23:17:27 +0530

    Yml to Release

--- verletjs/src/placeholder/advanced.ts
+++ verletjs/src/placeholder/advanced.ts
@@ -4,8 +4,3 @@ export function testMul (a: number, b: number) : number
 {
     return (a*b)
 }
-
-export function testDiv (a: number, b: number) : number
-{
-    return (a/b)
-}
--- verletjs/src/simple.ts
+++ verletjs/src/simple.ts
@@ -4,8 +4,3 @@ export function testSum (a: number, b: number) : number
 {
     return (a+b)
 }
-
-export function testSub (a: number, b: number) : number
-{
-    return (a-b)
-}
