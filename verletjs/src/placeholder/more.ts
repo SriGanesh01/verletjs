a28397826b834820e86f06d77429569b8fc60984
SriGanesh01 <psriganesh2005@gmail.com>
2025-07-10 23:19:59 +0530

    Release Test 1

--- verletjs/src/placeholder/more.ts
+++ verletjs/src/placeholder/more.ts
@@ -4,23 +4,3 @@ export function testMod (a: number, b: number) : number
 {
     return (a%b)
 }
-
-export function testPow (a: number, b: number) : number
-{
-    return (a**b)
-}
-
-export function testPowSum (a: number, b: number) : number
-{
-    return ((a**a) + (b**b))
-}
-
-export function IHHI (a: number, b: number) : number
-{
-    return ((a**a) + (b**b))
-}
-
-export function Cutee (a: number, b: number) : number
-{
-    return ((a**a) + (b**b))
-}
